1ad3f2231cd981d4cd6731cbb78f202340de22cf
StevenTCramer <Steven.Cramer@TimeWarp.Enterprises>
2020-05-09 13:47:04 +0000

    Allow command line and Hard code the default Boss.py Python app to my folder

--- src/test/java/Spring2020Main.java
+++ src/test/java/Spring2020Main.java
@@ -4,18 +4,68 @@ import com.codingame.gameengine.runner.MultiplayerGameRunner;
 
 public class Spring2020Main {
     public static void main(String[] args) {
-        MultiplayerGameRunner gameRunner = new MultiplayerGameRunner();
-        // Select agents here
-        gameRunner.addAgent("python3 config/Boss.py", "Blinky", "https://static.codingame.com/servlet/fileservlet?id=43829808065962");
-        gameRunner.addAgent("python3 config/Boss.py", "Inky", "https://static.codingame.com/servlet/fileservlet?id=43829821541064");
+        // read cli args
+        Options opts = new Options().read(args);
+        String cli1 = opts.getCommandLine1();
+        String cli2 = opts.getCommandLine2();
 
+        MultiplayerGameRunner gameRunner = new MultiplayerGameRunner();
+        gameRunner.setLeagueLevel(3);
         gameRunner.setSeed(5842184981578562716L);
+
+        // add player 1
+        if (null != cli1) {
+            gameRunner.addAgent(cli1);
+        } else {
+            gameRunner.addAgent("python C:/git/AzureDevOps/Training/CodingGame/PacMan/OtherBots/Boss.py", "Blinky", "https://static.codingame.com/servlet/fileservlet?id=43829808065962");
+        }
+
+        // add player 2
+        if (null != cli2) {
+            gameRunner.addAgent(cli2);
+        } else {
+            gameRunner.addAgent("python C:/git/AzureDevOps/Training/CodingGame/PacMan/OtherBots/Boss.py", "Inky", "https://static.codingame.com/servlet/fileservlet?id=43829821541064");
+        }
+
 
         Properties params = new Properties();
 
         gameRunner.setGameParameters(params);
-        gameRunner.setLeagueLevel(3);
 
         gameRunner.start(8888);
     }
 }
+
+class Options {
+    private String commandLine1 = null;
+    private String commandLine2 = null;
+
+    public Options read(String[] args) {
+        if (args == null) {
+            return this;
+        }
+
+        switch (args.length) {
+            case 1: {
+                this.commandLine1 = args[0];
+                break;
+            }
+            case 2: {
+                this.commandLine1 = args[0];
+                this.commandLine2 = args[1];
+                break;
+            }
+        }
+
+        return this;
+    }
+
+    public String getCommandLine1() {
+        return this.commandLine1;
+    }
+
+    public String getCommandLine2() {
+        return this.commandLine2;
+    }
+}
+
